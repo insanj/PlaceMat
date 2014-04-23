1a5ce367733c42f4798ea453d8769e0c764c3647
insanj <insanj@gmail.com>
2014-04-23 17:53:21 -0400

    polishing place vc (with toolbar magic!)

--- PlaceMat/MURPlaceViewController.h
+++ PlaceMat/MURPlaceViewController.h
@@ -8,11 +8,13 @@
 
 #import "MURTableViewController.h"
 #import "MURPlace.h"
+#import "MURBlurView.h"
 
 @interface MURPlaceViewController : UIViewController <UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout>
 
 @property(nonatomic, retain) MURPlace *place;
 @property(nonatomic, retain) UICollectionView *collectionView;
+@property(nonatomic, retain) UIToolbar *backingView;
 
 - (instancetype)initWithPlace:(MURPlace *)given;
 
--- PlaceMat/MURPlaceViewController.m
+++ PlaceMat/MURPlaceViewController.m
@@ -18,25 +18,32 @@
 	self.view.backgroundColor = [MURTheme backgroundColor];
 	
 	CGRect collectionFrame = self.view.bounds;
-	collectionFrame.size.height /= 2.0;
+	collectionFrame.size.height /= 1.75;
 	
 	UICollectionViewFlowLayout *layout = [[UICollectionViewFlowLayout alloc] init];
 	layout.scrollDirection = UICollectionViewScrollDirectionVertical;
 	
 	_collectionView = [[UICollectionView alloc] initWithFrame:collectionFrame collectionViewLayout:layout];
 	[_collectionView registerClass:UICollectionViewCell.class forCellWithReuseIdentifier:@"PlaceCell"];
-	_collectionView.backgroundColor = [UIColor colorWithWhite:0.85 alpha:1.0];
-	_collectionView.contentInset = UIEdgeInsetsMake(50.0, 0.0, 0.0, 0.0);
-	_collectionView.scrollIndicatorInsets = UIEdgeInsetsMake(64.0, 0.0, 0.0, 0.0);
+	_collectionView.backgroundColor = [UIColor colorWithWhite:0.9 alpha:1.0];
+	_collectionView.contentInset = UIEdgeInsetsMake(-20.0, 0.0, 0.0, 0.0);
+	_collectionView.scrollIndicatorInsets = UIEdgeInsetsMake(0.0, 0.0, 10.0, 0.0);
 	_collectionView.delegate = self;
 	_collectionView.dataSource = self;
 	[self.view addSubview:_collectionView];
-
+	
+	CGRect backingFrame = self.view.bounds;
+	backingFrame.origin.y += collectionFrame.origin.y + (self.view.bounds.size.height / 1.8);
+	backingFrame.size.height = self.view.bounds.size.height - backingFrame.origin.y;
+	
+	_backingView = [[UIToolbar alloc] initWithFrame:backingFrame];
+	[self.view addSubview:_backingView];
+	
 	return self;
 }
 
 - (instancetype)init {
-	NSLog(@"SHOULDN'T BE INITTING WITH DEFAULT CONSTRUCTOR!");
+	NSLog(@" *** MURPlaceViewController shouldn't be initialized using the default constructor!");
 	return ((self = [self initWithPlace:[[MURPlace alloc] initWithName:@"Connections"]]));
 }
 
@@ -44,29 +51,30 @@
     [super viewDidLoad];
 	
 	self.navigationItem.rightBarButtonItem = [[MURCheckinButtonItem alloc] initWithName:_place.name];
-
-	CGFloat side = (self.view.bounds.size.height / 2.0) - (_collectionView.frame.size.height + 100.0);
-	UIImageView *bigPlaceImage = [[UIImageView alloc] initWithFrame:CGRectMake(20.0, (self.view.bounds.size.height / 2.0) + 30.0, side, side)];
+	
+	CGFloat side = (_backingView.frame.size.height / 2.0);
+	UIImageView *bigPlaceImage = [[UIImageView alloc] initWithFrame:CGRectMake(20.0, 20.0, side, side)];
 	bigPlaceImage.contentMode = UIViewContentModeScaleAspectFill;
 	bigPlaceImage.layer.masksToBounds = YES;
 	bigPlaceImage.layer.cornerRadius = 10.0;
 	bigPlaceImage.image = _place.avatar;
-	[self.view addSubview:bigPlaceImage];
+	[_backingView addSubview:bigPlaceImage];
 	
-	UITextView *bigPlaceText = [[UITextView alloc] initWithFrame:CGRectMake(bigPlaceImage.frame.origin.x + bigPlaceImage.frame.size.width + 10.0, bigPlaceImage.frame.origin.y, self.view.bounds.size.width - (20.0 + bigPlaceImage.frame.size.width + 20.0), bigPlaceImage.frame.size.height)];
+	UITextView *bigPlaceText = [[UITextView alloc] initWithFrame:CGRectMake(bigPlaceImage.frame.origin.x + bigPlaceImage.frame.size.width + 10.0, bigPlaceImage.frame.origin.y, _backingView.frame.size.width - (20.0 + bigPlaceImage.frame.size.width + 20.0), bigPlaceImage.frame.size.height)];
 	bigPlaceText.editable = NO;
 	bigPlaceText.backgroundColor = [UIColor clearColor];
 	bigPlaceText.textColor = [UIColor blackColor];
 	bigPlaceText.font = [UIFont systemFontOfSize:14.0];
 	bigPlaceText.text = [NSString stringWithFormat:@"Welcome to %@. You can use %@ to buy %@ during %@.", [_place.name stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@" "]], _place.description, _place.serving, _place.time];
-	[self.view addSubview:bigPlaceText];
+	[_backingView addSubview:bigPlaceText];
 	
-	UILabel *legend = [[UILabel alloc] initWithFrame:CGRectMake(0.0, self.view.frame.size.height - 50.0, self.view.frame.size.width, 50.0)];
+	UILabel *legend = [[UILabel alloc] initWithFrame:CGRectMake(0.0, _backingView.frame.size.height - 50.0, _backingView.frame.size.width, 50.0)];
 	legend.textAlignment = NSTextAlignmentCenter;
 	legend.font = [UIFont systemFontOfSize:13.0];
 	legend.textColor = [UIColor lightGrayColor];
 	legend.text = @"Vegan: *     Vegetarian: ✭     Gluten-free: ✝";
-	[self.view addSubview:legend];
+	[_backingView addSubview:legend];
+
 }
 
 - (void)viewDidAppear:(BOOL)animated {
